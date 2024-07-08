20258cc064f27515a624636fd824f6f58319b518
Abdullah0095 <abdullah.web1995@gmail.com>
2024-07-08 07:56:54 +0600

    trying

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -12,44 +12,55 @@ const Navbar = () => {
   </>;
 
   return (
-    <div>
-      <div className="navbar bg-slate-900 z-[1] fixed top-0 left-0 right-0">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-accent btn-circle"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content text-lg bg-teal-700 rounded-box z-[1]  mt-3 w-40 p-2 text-white "
-            >
-              {navList}
-            </ul>
-          </div>
-        </div>
-        <div className="navbar-center">
-          <Link href="/" className="btn btn-ghost ml-[-30px] text-2xl text-white">sabrIT</Link>
-        </div>
-      </div>
+    // <div>
+    //   <div className="navbar bg-slate-900 z-[1] fixed top-0 left-0 right-0">
+    //     <div className="navbar-start">
+    //       <div className="dropdown">
+    //         <div
+    //           tabIndex={0}
+    //           role="button"
+    //           className="btn btn-accent btn-circle"
+    //         >
+    //           <svg
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             className="h-5 w-5"
+    //             fill="none"
+    //             viewBox="0 0 24 24"
+    //             stroke="currentColor"
+    //           >
+    //             <path
+    //               strokeLinecap="round"
+    //               strokeLinejoin="round"
+    //               strokeWidth="2"
+    //               d="M4 6h16M4 12h16M4 18h7"
+    //             />
+    //           </svg>
+    //         </div>
+    //         <ul
+    //           tabIndex={0}
+    //           className="menu menu-sm dropdown-content text-lg bg-teal-700 rounded-box z-[1]  mt-3 w-40 p-2 text-white "
+    //         >
+    //           {navList}
+    //         </ul>
+    //       </div>
+    //     </div>
+    //     <div className="navbar-center">
+    //       <Link href="/" className="btn btn-ghost ml-[-30px] text-2xl text-white">sabrIT</Link>
+    //     </div>
+    //   </div>
       
+    // </div>
+    <div className="navbar bg-slate-900 z-[1] fixed top-0 left-0 right-0">
+    <div className="navbar-start">
+      <Link href="/" className="text-white">Home</Link>
     </div>
+    <div className="navbar-center">
+      <Link href="/" className="text-white">sabrIT</Link>
+    </div>
+    <div className="navbar-end">
+      <Link href="/service" className="text-white">Service</Link>
+    </div>
+  </div>
   );
 };
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <body className={inter.className}>
         <Navbar />
         {children}
         </body>
@@ -20,4 +20,4 @@ export default function RootLayout({ children }) {
   );
 }
 
-// className={inter.className}
+
